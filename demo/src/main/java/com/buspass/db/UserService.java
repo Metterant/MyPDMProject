9df5f18b3d3bba1efe4a89e560dca0c1258d01a4
Metterant <metterantdev@gmail.com>
2025-11-17 02:09:46 +0700

    remove UserRoles enum and change it to UserRoleID instead

--- demo/src/main/java/com/buspass/db/UserService.java
+++ demo/src/main/java/com/buspass/db/UserService.java
@@ -6,34 +6,28 @@ import java.util.List;
 import java.util.Map;
 
 public class UserService {
-    public enum UserRoles {
-        Passenger("Passenger"),
-        Admin("Admin");
-
-        private final String text;
-
-        UserRoles(final String text) {
-            this.text = text;
-        }
-
-        @Override
-        public String toString() {
-            return text;
-        }
-    }
-
     //1: Đăng ký người dùng mới (Dùng INSERT)
-    public boolean registerUser(String username, Integer age, String phone, String address ,UserRoles userRole) {
-        String sql = "INSERT INTO user(Username, Age, Phone, Address, Role) " +
-                "VALUES (?, ?, ?, ?, ?)";
+    /**
+     * A method used to create a new user
+     * @param username
+     * @param age
+     * @param phone
+     * @param address
+     * @param userRoleID: Use dropdown menu (1 -> Passenger, 2 -> Admin)
+     * @return
+     */
+    public boolean registerUser(String username, int age, String phone, String address, int userRoleID) {
+        String sql = "INSERT INTO user(Username, UserPassword, Age, Phone, UserAddress, UserRoleID) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
         // Gọi phương thức executeUpdate từ module của nhóm
-        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, age, phone, address, userRole.toString());
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, age, phone, address, userRoleID);
         return rowsAffected > 0; // thành công--> True
     }
 
     //2: Lấy thông tin người dùng bằng ID (Dùng SELECT)
     public Map<String, Object> getUserById(int userId) {
-        String sql = "SELECT * FROM user WHERE UserID = ?";
+        String sql = "SELECT Username, Age, Phone, UserAddress, RoleDescription " + //
+            "FROM User JOIN UserRoles ON User.UserRoleID = UserRoles.UserRoleID WHERE UserID = ?";
         // Gọi phương thức executeQuery từ module của nhóm
         List<Map<String, Object>> user = QueryExecutionModule.executeQuery(sql, userId);
         if (!user.isEmpty()) {
