7084380472bd05a5b1723d454259d31839188878
Metterant <metterantdev@gmail.com>
2025-11-17 10:33:10 +0700

    add more update methods

--- demo/src/main/java/com/buspass/db/UserService.java
+++ demo/src/main/java/com/buspass/db/UserService.java
@@ -16,17 +16,17 @@ public class UserService {
      * @param userRoleID: Use dropdown menu (1 -> Passenger, 2 -> Admin)
      * @return
      */
-    public boolean registerUser(String username, int age, String phone, String address, int userRoleID) {
+    public boolean registerUser(String username, String password, int age, String phone, String address, int userRoleID) {
         String sql = "INSERT INTO user(Username, UserPassword, Age, Phone, UserAddress, UserRoleID) " +
                 "VALUES (?, ?, ?, ?, ?, ?)";
         // Gọi phương thức executeUpdate từ module của nhóm
-        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, age, phone, address, userRoleID);
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, password , age, phone, address, userRoleID);
         return rowsAffected > 0; // thành công--> True
     }
 
     //2: Lấy thông tin người dùng bằng ID (Dùng SELECT)
     public Map<String, Object> getUserById(int userId) {
-        String sql = "SELECT Username, Age, Phone, UserAddress, RoleDescription " + //
+        String sql = "SELECT UserID, Username, Age, Phone, UserAddress, RoleDescription " + //
             "FROM User JOIN UserRoles ON User.UserRoleID = UserRoles.UserRoleID WHERE UserID = ?";
         // Gọi phương thức executeQuery từ module của nhóm
         List<Map<String, Object>> user = QueryExecutionModule.executeQuery(sql, userId);
@@ -34,5 +34,83 @@ public class UserService {
             return user.get(0); // Trả về người dùng đầu tiên tìm thấy
         }
         return null; // Không tìm thấy
+    }
+
+    /**
+     * Change the name of the user
+     * @param userId the UserID of User table
+     * @param username the new name of the user
+     * @return true if the user is found and their name was changed successfully
+     */
+    public boolean updateUsername(Integer userId, String username) {
+        String sql = "UPDATE user SET Username = ? " +
+                "WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username);
+        return rowsAffected > 0;
+    }
+
+    /**
+     * Change the password of the user
+     * @param userId the UserID of User table
+     * @param newPassword the new password of the user
+     * @return true if the user is found and their password was changed successfully
+     */
+    public boolean updatePassword(Integer userId, String newPassword) {
+        String sql = "UPDATE user SET UserPassword = ? " +
+                "WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, newPassword);
+        return rowsAffected > 0;
+    }
+
+    /**
+     * Change the address of the user
+     * @param userId the UserID of User table
+     * @param address the new address of the user
+     * @return true if the user is found and their address was changed successfully
+     */
+    public boolean updateAddress(Integer userId, String address) {
+        String sql = "UPDATE user SET UserAddreess = ? " +
+                "WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, address);
+        return rowsAffected > 0;
+    }
+
+    public boolean updateAge(Integer userId, int address) {
+        String sql = "UPDATE user SET Age = ? " +
+                "WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, address);
+        return rowsAffected > 0;
+    }
+
+    /**
+     * Change the UserRoleID of the user
+     * @param userId the UserID of User table
+     * @param address the new UserRoleID of the user corresponding to the new Role (1: Passenger, 2: Admin)
+     * @return true if the user is found and their UserRoleID was changed successfully
+     */
+    public boolean updateRoleID(Integer userId, int userRoleId) {
+        String sql = "UPDATE user SET UserRoleID = ? " +
+                "WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, userRoleId);
+        return rowsAffected > 0;
+    }
+
+    /**
+     * Delete User from the User Table by UserID
+     * TODO: Make sure to implement confirmation to aovid accidential terminiation
+     * 
+     * @param userId the UserID of the User table
+     * @return whether the User is found and their profile was deleted successfully
+     */
+    public boolean deleteUser(Integer userId) {
+        String sql = "DELETE FROM user WHERE UserID = " + userId.toString();
+        
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql);
+        return rowsAffected > 0;
     }
 }
