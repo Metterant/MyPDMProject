988fb43b37462519a0e2527ffe25c7038df18b85
Metterant <metterantdev@gmail.com>
2025-11-18 03:24:43 +0700

    add password hashing for user registration and update

--- demo/src/main/java/com/buspass/queries/UserService.java
+++ demo/src/main/java/com/buspass/queries/UserService.java
@@ -1,45 +1,48 @@
-//Khi muốn thêm một người dùng mới
-
 package com.buspass.queries;
 
 import java.util.List;
 import java.util.Map;
 
 import com.buspass.db.QueryExecutionModule;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UserService {
     //1: Đăng ký người dùng mới (Dùng INSERT)
     /**
      * A method used to create a new user
-     * @param username
+     * @param userName
      * @param age
      * @param phone
      * @param address
      * @param userRoleID: Use dropdown menu (1 -> Passenger, 2 -> Admin)
      * @return
      */
-    public boolean registerUser(String username, String password, int age, String phone, String address, int userRoleID) {
-        String sql = "INSERT INTO user(Username, UserPassword, Age, Phone, UserAddress, UserRoleID) " +
+    public boolean registerUser(String userName, String plainTextPassword, int age, String phone, String address, int userRoleID) {
+        String sql = "INSERT INTO user(UserName, UserPassword, Age, Phone, UserAddress, UserRoleID) " +
                 "VALUES (?, ?, ?, ?, ?, ?)";
-        // Gọi phương thức executeUpdate từ module của nhóm
-        int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, password , age, phone, address, userRoleID);
+
+        String hashedPassword = BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, userName, hashedPassword , age, phone, address, userRoleID);
         return rowsAffected > 0;
     }
 
     //2: Lấy thông tin người dùng bằng ID (Dùng SELECT)
     public Map<String, Object> getUserById(int userId) {
-        String sql = "SELECT UserID, Username, Age, Phone, UserAddress, RoleDescription " + //
+        String sql = "SELECT UserID, UserName, Age, Phone, UserAddress, RoleDescription " + //
             "FROM User JOIN UserRoles ON User.UserRoleID = UserRoles.UserRoleID WHERE UserID = ?";
         // Gọi phương thức executeQuery từ module của nhóm
-        List<Map<String, Object>> user = QueryExecutionModule.executeQuery(sql, userId);
-        if (!user.isEmpty()) {
-            return user.get(0); // Trả về người dùng đầu tiên tìm thấy
+        List<Map<String, Object>> users = QueryExecutionModule.executeQuery(sql, userId);
+        if (!users.isEmpty()) {
+            return users.get(0); // Trả về người dùng đầu tiên tìm thấy
         }
         return null; // Not found
     }
 
+    /* ADMIN PRIVILEDGES */
+
     public List<Map<String, Object>> getAllUsers() {
-        String sql = "SELECT UserID, Username, Age, Phone, UserAddress, RoleDescription " + //
+        String sql = "SELECT UserID, UserName, Age, Phone, UserAddress, RoleDescription " + //
             "FROM User JOIN UserRoles ON User.UserRoleID = UserRoles.UserRoleID";
             
         List<Map<String, Object>> users = QueryExecutionModule.executeQuery(sql);
@@ -53,8 +56,8 @@ public class UserService {
      * @param username the new name of the user
      * @return true if the user is found and their name was changed successfully
      */
-    public boolean updateUsername(int userId, String username) {
-        String sql = "UPDATE User SET Username = ? WHERE UserID = ?";
+    public boolean updateUserName(int userId, String username) {
+        String sql = "UPDATE User SET UserName = ? WHERE UserID = ?";
         int rowsAffected = QueryExecutionModule.executeUpdate(sql, username, userId);
         return rowsAffected > 0;
     }
@@ -62,12 +65,14 @@ public class UserService {
     /**
      * Change the password of the user
      * @param userId the UserID of User table
-     * @param newPassword the new password of the user
+     * @param plainTextPassword the new password of the user
      * @return true if the user is found and their password was changed successfully
      */
-    public boolean updatePassword(int userId, String newPassword) {
+    public boolean updatePassword(int userId, String plainTextPassword) {
+        String hashedPassword = BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+
         String sql = "UPDATE User SET UserPassword = ? WHERE UserID = ?";
-        int rowsAffected = QueryExecutionModule.executeUpdate(sql, newPassword, userId);
+        int rowsAffected = QueryExecutionModule.executeUpdate(sql, hashedPassword, userId);
         return rowsAffected > 0;
     }
 
