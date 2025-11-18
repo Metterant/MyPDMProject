fe4584e3cc207a14da065bbb4c0279183766bf4e
Metterant <metterantdev@gmail.com>
2025-11-18 21:43:23 +0700

    temporary changes

--- src/main/java/com/buspass/queries/DriverQuery.java
+++ src/main/java/com/buspass/queries/DriverQuery.java
@@ -4,6 +4,9 @@ public class DriverQuery {
     
     //#region ADMIN PRIVILEDGES
     
+    public boolean registerDriver(String name, String liscense) {
+        return false;
+    }
 
     //#endregion
 }
