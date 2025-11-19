cae5129b316460a4664cf10697eba6001771a9a3
Metterant <metterantdev@gmail.com>
2025-11-19 11:44:58 +0700

    add a method that convert minutes to sql.Time

--- src/main/java/com/buspass/utils/MathUtils.java
+++ src/main/java/com/buspass/utils/MathUtils.java
@@ -1,5 +1,17 @@
 package com.buspass.utils;
 
+import java.sql.Time;
+import java.time.LocalTime;
+
 public class MathUtils {
-    
+    public static Time convertToTimeString(int durationInMinutes) {
+        // Convert duration in minutes to SQL TIME (HH:MM:SS)
+		int hours = durationInMinutes / 60;
+		int minutes = durationInMinutes % 60;
+		// LocalTime requires hour < 24; durations longer than 24h are not expected for routes.
+		LocalTime lt = LocalTime.of(hours, minutes);
+		Time sqlTime = Time.valueOf(lt);
+
+        return sqlTime;
+    }
 }
