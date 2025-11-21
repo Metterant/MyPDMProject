98d626cc8adb56404b81b36f53cf19548acffc5d
Metterant <metterantdev@gmail.com>
2025-11-21 21:18:14 +0700

    fix sql string in getAllTrips method

--- src/main/java/com/buspass/queries/TripQuery.java
+++ src/main/java/com/buspass/queries/TripQuery.java
@@ -33,8 +33,8 @@ public class TripQuery {
     }
 
     public List<Map<String, Object>> getAllTrips() {
-        String sql = "SELECT * FROM Trip" +
-                     "ORDER BY TripDate, DepartureTime";;
+        String sql = "SELECT * FROM Trip\r\n" +
+                     "ORDER BY TripDate, DepartureTime";
         return QueryExecutionModule.executeQuery(sql);
     }
 
