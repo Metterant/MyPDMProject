76719f99a98c0906ea96ddf08a451ff935dd47ea
Metterant <metterantdev@gmail.com>
2025-11-21 14:46:05 +0700

    feat: add dialog methods for user registration feedback

--- src/main/java/com/buspass/utils/DialogUtils.java
+++ src/main/java/com/buspass/utils/DialogUtils.java
@@ -1,5 +1,13 @@
 package com.buspass.utils;
 
+import javax.swing.JOptionPane;
+
 public class DialogUtils {
-    
+    public static void showDialogUserAlreadyExists(String username) {
+        JOptionPane.showMessageDialog(null, "User with Username: " + username + " already exists.", "Registration Failed", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void showDialogRegistrationSuccess(String username) {
+        JOptionPane.showMessageDialog(null, "Registration Successful with Username: " + username, "Registration Successful", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
